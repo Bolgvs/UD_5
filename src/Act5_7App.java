78f230d5c9bd4bcc67d85ee1f2d1325fd1503f06
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-07 10:34:49 +0200

    Fin ex 7

---
+++ src/Act5_7App.java
@@ -0,0 +1,17 @@
+public class Act5_7App {
+	
+	public static void main (String args []) {
+		
+		int num = 1;
+		
+		while (num <= 100) {
+		
+		System.out.println(num);
+		
+		num ++;
+		
+		}
+		
+	}
+
+}
