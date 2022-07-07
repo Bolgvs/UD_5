21c370042654a7b3df219b9136d62884d5573325
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-07 10:30:14 +0200

    Fin ejercicio 6

--- src/Act5_6App.java
+++ src/Act5_6App.java
@@ -4,11 +4,23 @@ public class Act5_6App {
 	
 	public static void main (String args []) {
 		
-		// constante IVA
+		// Constante IVA
 		
 		final double IVA = 0.21;
 		
+		// Petición del precio:
+		
 		String texto_precio = JOptionPane.showInputDialog(null, "¿Cuál es el precio?");
+		
+		// 	Conversión de String - Double
+		
+		double precio = Double.parseDouble(texto_precio);
+		
+		// Cálculo precio final:
+		
+		double PF = precio + ( precio * IVA );
+		
+		JOptionPane.showMessageDialog(null, "El precio final es de: " + PF);
 		
 	}
 }
