6dce1fba186d5ea26f7a74babf7b318a7d3d48bf
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-07 11:55:02 +0200

    Commit Ex 12

--- src/Act5_12App.java
+++ src/Act5_12App.java
@@ -1,8 +1,24 @@
+import javax.swing.JOptionPane;
+
 public class Act5_12App {
 	
 	public static void main (String args []) {
 		
+		String contraseña = "Bolgvs", peticion;
 		
+		boolean mensaje = false;
+		
+		for (int intentos = 3; intentos != 0 && !mensaje; intentos--) {
+			
+			peticion = JOptionPane.showInputDialog("Escribe la contraseña:");
+			
+			if (peticion.equals(contraseña)) {
+				
+				mensaje = true;
+				
+			}
+			
+		}
 		
 	}
 
