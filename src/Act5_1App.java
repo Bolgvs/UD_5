dfc429c29c7d9414af6091fa0008f051ec8aad1f
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-07 09:26:42 +0200

    Primer commit

---
+++ src/Act5_1App.java
@@ -0,0 +1,8 @@
+public class Act5_1App {
+	
+	public static void main (String args []) {
+		
+		
+	}
+
+}
---
+++ src/Act5_2App.java
@@ -0,0 +1,9 @@
+public class Act5_2App {
+	
+	public static void main (String args []) {
+		
+		int num1 = 67, num2 = 5;
+		
+	}
+
+}
