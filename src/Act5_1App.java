2cd2c9f7b7ee9650187abc861dbe90bc87a95c20
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-07 09:36:24 +0200

    Commit ejercicio 1

--- src/Act5_1App.java
+++ src/Act5_1App.java
@@ -2,6 +2,20 @@ public class Act5_1App {
 	
 	public static void main (String args []) {
 		
+		int num1 = 77, num2 = 91;
+		
+		if (num1 > num2) {
+			
+			System.out.print("El primer número es mayor que el segundo");
+			
+		} else if ( num1 == num2 ) {
+			
+			System.out.print("El primer número es igual que el segundo");
+			
+		} else {
+			
+			System.out.print("El primer número es menor que el segundo");
+		}
 		
 	}
 
--- src/Act5_2App.java
+++ src/Act5_2App.java
@@ -2,7 +2,7 @@ public class Act5_2App {
 	
 	public static void main (String args []) {
 		
-		int num1 = 67, num2 = 5;
+		
 		
 	}
 
